4422e9115aee549e79f6cb27ec009bb9c40057cc
Alex Light <alex.thgil@gmail.com>
2019-10-13 14:53:52 +0300

    initial commit
    initial commit

---
+++ Cinema/Cinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Alex on 10/13/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

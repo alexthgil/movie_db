8bbf50fde62f2d85ed7d5677286b3d456c38e444
Alex Light <alex.thgil@gmail.com>
2019-10-13 22:08:54 +0300

    add SceneDelegate

---
+++ Cinema/Cinema/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  Cinema
+//
+//  Created by Alex on 10/13/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
